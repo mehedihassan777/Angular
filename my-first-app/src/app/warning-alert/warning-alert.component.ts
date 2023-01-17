873e777b747e7a506da2603d01f6266956586b8a
Mehedi Hassan <mehedi15-5815@diu.edu.bd>
2023-01-17 12:27:31 +0600

    1st Assignment

---
+++ my-first-app/src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-warning-alert',
+    template: `<p>This is a warning, You are in danger!</p>`,
+    styles: [`
+        p{
+            padding: 20px;
+            background-color: lightcoral;
+            border: 1px solid black;
+        }
+    `]
+})
+export class WarningAlertComponent {
+
+}
