4273e6bad16f57c7c9282a3a1a8433ebbe1b881e
Mehedi Hassan <mehedi15-5815@diu.edu.bd>
2023-01-17 16:14:18 +0600

    ngStyle Directive

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -9,7 +9,15 @@ export class ServerComponent {
     serverId: number = 10;
     serverStatus: string = 'Offline';
 
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'Online' : 'Offline';
+    }
+
     getServerStatus() {
         return this.serverStatus;
+    }
+
+    getColor() {
+        return this.serverStatus === 'Online' ? 'green' : 'red';
     }
 }
