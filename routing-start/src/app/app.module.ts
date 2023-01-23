246a3f00684e96fed340a99b31793051baf9a714
Mehedi Hassan <mehedi15-5815@diu.edu.bd>
2023-01-23 12:42:10 +0600

    Redirect wildcard route

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -12,11 +12,23 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'users', component: UserComponent },
-  { path: 'servers', component: ServersComponent }
+  {
+    path: 'users', component: UsersComponent, children: [
+      { path: ':id/:name', component: UserComponent }
+    ]
+  },
+  {
+    path: 'servers', component: ServersComponent, children: [
+      { path: ':id', component: ServerComponent },
+      { path: ':id/edit', component: EditServerComponent }
+    ]
+  },
+  { path: 'not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/not-found' }
 ];
 
 @NgModule({
@@ -27,7 +39,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
--- routing-start/src/app/home/home.component.ts
+++ routing-start/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ServersService } from '../servers/servers.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  private servers: { id: number, name: string, status: string }[] = [];
 
-  constructor() { }
+  constructor(private serverService: ServersService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.servers = this.serverService.getServers();
+  }
+
+  loadServer(id: number) {
+    //this.router.navigate(['servers'], { relativeTo: this.route });
+    this.router.navigate(['/servers', id,'edit'], {queryParams: {allowEdit:'1'}, fragment: 'Loading'});
   }
 
 }
