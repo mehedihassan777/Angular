08a3e48bb437bd49fc217e60407dac091bb28b16
Mehedi Hassan <mehedi15-5815@diu.edu.bd>
2023-01-24 13:36:31 +0600

    Two way Binding in Form

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -9,6 +9,7 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
 
   @ViewChild('f') signupForm: NgForm;
+  answer = '';
 
   suggestUserName() {
     const suggestedName = 'Superuser';
