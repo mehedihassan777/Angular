a85fb334fb3edd6618476810f88ff3875c1ab8ec
Mehedi Hassan <mehedi15-5815@diu.edu.bd>
2023-01-24 11:14:27 +0600

    Observable Operator

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, Subscription, Observable } from 'rxjs';
+import { interval, Subscription, Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -16,15 +16,22 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
+        if (count === 5)
+          observer.complete();
         if (count > 3)
           observer.error(new Error('Count is greater than !'));
         count++;
       }, 1000);
     });
-    this.firstObsSub = customObservable.subscribe(data => console.log(data), error => {
+    // this.firstObsSub = customObservable.subscribe(data => console.log(data), error => {
+    //   console.log(error);
+    //   alert(error.message);
+    // }, () => console.log('Complete'));
+
+    this.firstObsSub = customObservable.pipe(map((data: number) => { return 'Round: ' + (data + 1); })).subscribe(data => console.log(data), error => {
       console.log(error);
       alert(error.message);
-    });
+    }, () => console.log('Complete'));
   }
 
   ngOnDestroy(): void {
