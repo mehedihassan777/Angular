f97bc0bf03e94e05648eb2ccef2963fb13a4571b
Mehedi Hassan <mehedi15-5815@diu.edu.bd>
2023-01-25 13:50:12 +0600

    New Recipe Form start

--- project1/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ project1/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,12 +11,35 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
-  constructor(private route: ActivatedRoute) { }
+  recipeForm: FormGroup;
+  inputImgScr: string;
+  constructor(private route: ActivatedRoute, private recSer: RecipeService) { }
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
       this.id = +params['id'];
       this.editMode = params['id'] != null;
       console.log(this.editMode);
+      this.initForm();
     });
+  }
+
+  private initForm() {
+    let recipeName = '';
+    let dis = '';
+    if (this.editMode) {
+      recipeName = this.recSer.getRecipe(this.id).name;
+      this.inputImgScr = this.recSer.getRecipe(this.id).imagePath;
+      dis = this.recSer.getRecipe(this.id).description;
+    }
+
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName),
+      'inputImgScr': new FormControl(this.inputImgScr),
+      'dis': new FormControl(dis)
+    });
+  }
+
+  onSubmit() {
+
   }
 }
