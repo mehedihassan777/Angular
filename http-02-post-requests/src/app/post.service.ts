88d2145fb9738020191dafebe70d894c62cac36f
Mehedi Hassan <mehedi15-5815@diu.edu.bd>
2023-01-26 13:14:15 +0600

    Custom Header

--- http-02-post-requests/src/app/post.service.ts
+++ http-02-post-requests/src/app/post.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { catchError, map, Subject, throwError } from "rxjs";
 import { Post } from "./post.model";
@@ -16,7 +16,9 @@ export class PostService {
     }
 
     fetchPosts() {
-        return this.http.get<{ [key: string]: Post }>('https://http-practice-9d5c8-default-rtdb.firebaseio.com/posts.json').pipe(map(responseData => {
+        return this.http.get<{ [key: string]: Post }>('https://http-practice-9d5c8-default-rtdb.firebaseio.com/posts.json', {
+            headers: new HttpHeaders({ 'Custom-Header': 'Hello' })
+        }).pipe(map(responseData => {
             const postArray: Post[] = [];
             for (const key in responseData) {
                 if (responseData.hasOwnProperty(key))
