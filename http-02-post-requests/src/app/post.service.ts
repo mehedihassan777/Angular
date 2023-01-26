b13b5a54fcbe1ff61c61502f67320279c1ed4a93
Mehedi Hassan <mehedi15-5815@diu.edu.bd>
2023-01-26 13:39:08 +0600

    Observe in Http Request

--- http-02-post-requests/src/app/post.service.ts
+++ http-02-post-requests/src/app/post.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpEventType, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { catchError, map, Subject, throwError } from "rxjs";
+import { catchError, map, Subject, tap, throwError } from "rxjs";
 import { Post } from "./post.model";
 
 @Injectable({ providedIn: 'root' })
@@ -10,14 +10,24 @@ export class PostService {
 
     createAndStorePost(title: string, content: string) {
         const postData: Post = { title: title, content: content };
-        this.http.post<{ name: string }>('https://http-practice-9d5c8-default-rtdb.firebaseio.com/posts.json', postData).subscribe(responseData => {
+        this.http.post<{ name: string }>('https://http-practice-9d5c8-default-rtdb.firebaseio.com/posts.json', postData, {
+            
+            observe: 'response'
+            
+        }).subscribe(responseData => {
             console.log(responseData);
         }, error => { this.error.next(error.message) });
     }
 
     fetchPosts() {
+        let searchParams = new HttpParams();
+        searchParams = searchParams.append('print', 'pretty');
+        searchParams = searchParams.append('custom', 'key');
         return this.http.get<{ [key: string]: Post }>('https://http-practice-9d5c8-default-rtdb.firebaseio.com/posts.json', {
-            headers: new HttpHeaders({ 'Custom-Header': 'Hello' })
+            headers: new HttpHeaders({ 'Custom-Header': 'Hello' }),
+            //params: new HttpParams().set('print', 'pretty')
+            params: searchParams
+
         }).pipe(map(responseData => {
             const postArray: Post[] = [];
             for (const key in responseData) {
@@ -33,7 +43,19 @@ export class PostService {
     }
 
     deletePosts() {
-        return this.http.delete('https://http-practice-9d5c8-default-rtdb.firebaseio.com/posts.json');
+        return this.http.delete('https://http-practice-9d5c8-default-rtdb.firebaseio.com/posts.json', {
+
+            observe: 'events'
+            
+        }).pipe(tap(event => {
+            console.log(event);
+            if (event.type === HttpEventType.Sent) {
+                
+            }
+            if (event.type === HttpEventType.Response) {
+                console.log(event.body);
+            }
+        }));
     }
 
 }
